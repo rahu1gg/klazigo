83000063726fff6bea648ed63e9881d700014c0e
Rahul Palamarthi <rahulpalamarthi@gmail.com>
2022-07-04 11:01:40 +0530

    cart page and data for cart page

---
+++ src/Pages/Cart.js
@@ -0,0 +1,198 @@
+/* eslint-disable array-callback-return */
+/* eslint-disable no-unused-vars */
+
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import MedicineItemInCart from "./Data/Medicineitemincart";
+import trashCanIcon from "../Images/trashCan.png";
+import coupon from "../Images/Coupon.png";
+import location from "../Images/location.png";
+
+const CartMedicineDetail = (props) => {
+	return (
+		<>
+			<div className="cartMedicine__container">
+				<div>
+					<p className="cartMedicine__name">{props.name}</p>
+					<p className="cartMedicine__priceDetails">
+						<span className="cartMedicine__rupeeLogo">₹</span>
+						{props.sellingPrice}
+						<br />
+						<span>₹{props.costPrice}</span>
+					</p>
+				</div>
+				<p>{props.packetInfo}</p>
+				<div className="cartMedicine__footer">
+					<p onClick={() => props.removeCartItem(props.id)}>
+						<img src={trashCanIcon} alt="removeIcon" /> Remove
+					</p>
+					<p>
+						<button onClick={() => props.decrement(props.id)}>-</button>
+						<span>{props.count}</span>
+						<button onClick={() => props.increment(props.id)}>+</button>
+					</p>
+				</div>
+			</div>
+		</>
+	);
+};
+
+function Cart() {
+	const [cartItems, setCartItems] = useState(
+		MedicineItemInCart.map((val) => ({
+			...val,
+			totalCostPrice: val.count * val.costPrice,
+			totalSellingPrice: val.count * val.sellingPrice,
+		}))
+	);
+	const [sellingPriceOfAllItems, setSellingPriceOfAllItems] = useState();
+	const [costPriceOfAllItems, setCostPriceOfAllItems] = useState();
+	const [shippingCharges, setShippingCharges] = useState(99);
+
+	const removeCartItem = (id) => {
+		setCartItems(
+			cartItems.filter((val) => {
+				if (val.id === id) {
+					const result = window.confirm(
+						"Are You Sure, You Want to Remove the Item from the Cart!!"
+					);
+					if (result) {
+						return;
+					}
+					// return val;
+				}
+				return val;
+			})
+		);
+	};
+
+	const increment = (id) => {
+		setCartItems(
+			cartItems.map((val) => {
+				if (val.id === id) {
+					return {
+						...val,
+						count: val.count + 1,
+						totalCostPrice: (val.count + 1) * val.costPrice,
+						totalSellingPrice: (val.count + 1) * val.sellingPrice,
+					};
+				}
+				return val;
+			})
+		);
+	};
+
+	const decrement = (id) => {
+		setCartItems(
+			cartItems.map((val) => {
+				if (val.id === id) {
+					return {
+						...val,
+						count: val.count > 1 ? val.count - 1 : 1,
+						totalCostPrice: (val.count > 1 ? val.count - 1 : 1) * val.costPrice,
+						totalSellingPrice: (val.count > 1 ? val.count - 1 : 1) * val.sellingPrice,
+					};
+				}
+				return val;
+			})
+		);
+	};
+
+	useEffect(() => {
+		const sellingPriceForAllItems = cartItems.reduce(
+			(sum, val) => sum + val.totalSellingPrice,
+			0
+		);
+		setSellingPriceOfAllItems(sellingPriceForAllItems);
+
+		const costPriceForAllItems = cartItems.reduce((sum, val) => sum + val.totalCostPrice, 0);
+		setCostPriceOfAllItems(costPriceForAllItems);
+	}, [cartItems]);
+
+	const CartMedicineDetailComponent = cartItems.map((val) => (
+		<CartMedicineDetail
+			key={val.id}
+			id={val.id}
+			name={val.name}
+			sellingPrice={val.sellingPrice}
+			costPrice={val.costPrice}
+			count={val.count}
+			packetInfo={val.packetInfo}
+			increment={increment}
+			decrement={decrement}
+			removeCartItem={removeCartItem}
+		/>
+	));
+
+	return (
+		<>
+			<section>
+				<div className="cartMedicineItems__container">
+					<p>Items({cartItems.length})</p>
+					<div>{CartMedicineDetailComponent}</div>
+					<div className="cartMedicineItems__totalPricing">
+						<p>
+							<img src={coupon} alt="couponImg" />
+							<span>Apply Coupon</span>
+						</p>
+						<div>
+							<div>
+								<p>Item Total(MRP)</p>
+								<p>
+									<span>₹</span>
+									{costPriceOfAllItems}
+								</p>
+							</div>
+							<div className="cartMedicineItems__discount">
+								<p>Price Discount</p>
+								<p>
+									<span>₹</span>
+									{costPriceOfAllItems - sellingPriceOfAllItems}
+								</p>
+							</div>
+							<hr />
+							<div>
+								<p>Shipping Fee</p>
+								<p>
+									<span>₹</span> {shippingCharges}
+								</p>
+							</div>
+							<div>
+								<p>Packaging Fee</p>
+								<p>
+									<span>₹</span> 0
+								</p>
+							</div>
+							<hr />
+							<div className="cartMedicineItems__toBePaid">
+								<p>To Be Paid</p>
+								<p>
+									<span>₹</span>
+									{sellingPriceOfAllItems + shippingCharges}
+								</p>
+							</div>
+							<div>
+								<p>Total Saving</p>
+								<p>
+									<span>₹</span>
+									{costPriceOfAllItems - sellingPriceOfAllItems}
+								</p>
+							</div>
+						</div>
+						<div className="cartMedicineItems__address">
+							<p>Delivery Address</p>
+							<p>
+								<img src={location} alt="LocationImg" /> New Delhi
+							</p>
+						</div>
+						<Link className="cartMedicineItems__proceed" to="/">
+							Proceed to Check Out
+						</Link>
+					</div>
+				</div>
+			</section>
+		</>
+	);
+}
+
+export default Cart;
---
+++ src/Pages/Data/Medicineitemincart.js
@@ -0,0 +1,27 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default [
+	{
+		id: 1,
+		name: "Accu-chek Active Blood Glucometer Kit( Box of 10 strips Free)",
+		sellingPrice: 1279,
+		costPrice: 1500,
+		packetInfo: "Packet of 1 Kit",
+		count: 1,
+	},
+	{
+		id: 2,
+		name: "Limcee Chewable Tablet Orange",
+		sellingPrice: 129,
+		costPrice: 150,
+		packetInfo: "Strip of 15 chewable tablets \nMinumum order quantity:3",
+		count: 1,
+	},
+	{
+		id: 3,
+		name: "Accu-chek Active Blood Glucometer Kit( Box of 10 strips Free)",
+		sellingPrice: 1279,
+		costPrice: 1500,
+		packetInfo: "Packet of 1 Kit",
+		count: 1,
+	},
+];
